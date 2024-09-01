7d2cede16880aae3cabb872776b12e5318f7b5f1
hritik <rohan.kinshu@gmail.com>
2024-09-01 18:11:39 +0530

    ..

---
+++ ConfigServer/src/test/java/com/Sping_kubernetes/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sping_kubernetes.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
